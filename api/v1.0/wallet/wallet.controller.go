78018d4e2c5c64a3a674674257de5ae8bc9dab95
Tomás Duarte <tomasrsduarte@gmail.com>
2019-05-13 14:30:50 +0100

    Add wallet ctrl

--- api/v1.0/wallet/wallet.controller.go
+++ api/v1.0/wallet/wallet.controller.go
@@ -27,7 +27,29 @@ func getBalance(c *gin.Context) {
 }
 
 func withdrawal(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
 
+	type Body struct {
+		Id int `json:"id" binding:"required"`
+		Amount float64 `json:"amount" binding:"required"`
+	}
+
+	var body Body
+
+	if err := c.BindJSON(&body); err != nil{
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	var exists Wallet
+
+	if err := db.Where("id = ?", body.Id).First(&exists).Error; err == nil{
+		c.JSON(http.StatusConflict, common.JSON{
+			"message": "Could not find wallet with id " + string(body.Id),
+		})
+
+		return
+	}
 }
 
 func deposit(c *gin.Context) {
