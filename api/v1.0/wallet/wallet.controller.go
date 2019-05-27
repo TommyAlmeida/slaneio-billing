cc1a3ca9577ba5ea814d007520c256931a4960b5
Tomás Duarte <tomasrsduarte@gmail.com>
2019-05-27 16:30:00 +0100

    Add filter by id method to wallet controller

--- api/v1.0/wallet/wallet.controller.go
+++ api/v1.0/wallet/wallet.controller.go
@@ -12,6 +12,11 @@ type Wallet = models.Wallet
 type User = models.User
 type JSON = common.JSON
 
+type WithdrawlDepositBody struct {
+	Id int `json:"id" binding:"required"`
+	Amount float64 `json:"amount" binding:"required"`
+}
+
 func getBalance(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	ownerId := c.Param("ownerId")
@@ -26,32 +31,49 @@ func getBalance(c *gin.Context) {
 	c.JSON(http.StatusOK, wallet.Serialize())
 }
 
+func GetWalletById(c *gin.Context, id int) (Wallet, error) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	var exists Wallet
+
+	if err := db.Where("id = ?", id).First(&exists).Error; err == nil{
+		return nil, fmt.Errorf("Could not find wallet with id %d", id)
+	}
+
+	return exists, nil
+}
+
 func withdrawal(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 
-	type Body struct {
-		Id int `json:"id" binding:"required"`
-		Amount float64 `json:"amount" binding:"required"`
-	}
-
-	var body Body
+	var body WithdrawlDepositBody
 
 	if err := c.BindJSON(&body); err != nil{
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
-	var exists Wallet
+	wallet, err := GetWalletById(body.Id)
 
-	if err := db.Where("id = ?", body.Id).First(&exists).Error; err == nil{
+	if err != nil {
 		c.JSON(http.StatusConflict, common.JSON{
 			"message": "Could not find wallet with id " + string(body.Id),
 		})
-
 		return
 	}
+
+	fmt.Printf("Wallet %+T", wallet)
 }
 
 func deposit(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
 
+	var body WithdrawlDepositBody
+
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	var exists
 }
